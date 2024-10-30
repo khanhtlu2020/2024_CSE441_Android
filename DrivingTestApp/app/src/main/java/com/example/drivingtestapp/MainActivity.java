2a0bd2e100c7a9cc76cf7b22f08d71ae1bc9d7fd
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-30 16:09:00 +0700

    Refined UI

--- DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
+++ DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
@@ -1,69 +1,32 @@
 package com.example.drivingtestapp;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.drivingtestapp.models.DataBase;
-
 public class MainActivity extends AppCompatActivity {
-//    private ArrayList<Question> questions = new ArrayList<>();
-//    DataBase dataBase = new DataBase(this,null,null,1);
 
-    Button btnStart,btnTrain,btnHistory,btnMore, btnThithu;
-    TextView txtRs,txtRsMax;
+    Button btnExam, btnMore;
 
-    DataBase dataBase;
-
-    int userId;
-
-    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult o) {
-                    if(o.getResultCode() == RESULT_OK){
-                        Intent data = o.getData();
-                        int rs = data.getIntExtra("result",0);
-                        txtRs.setText("Result: "+rs);
-                        dataBase.QuerySetData("insert into results(user_id,result) values("+userId+","+rs+")");
-                    }
-                }
-            }
-    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        map();
+        btnExam = findViewById(R.id.btnExam);
+        btnMore = findViewById(R.id.btnMore);
 
-        // Gọi database
-        dataBase = new DataBase(this, "user.sqlite", null, 1);
-
-        // Lấy dữ liệu từ người dùng đăng nhập
-        getUserData();
-
-        showResult();
-
-        btnThithu.setOnClickListener(new View.OnClickListener() {
+        btnExam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ExamListActivity.class);
                 startActivity(intent);
             }
         });
+
         btnMore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,40 +37,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void showResult() {
-        // Tạo bảng lưu kết quả
-        String createTBUser = "create table if not exists results (id integer primary key autoincrement, user_id integer REFERENCES users(id), result integer)";
-        dataBase.QuerySetData(createTBUser);
-
-        // Lấy dữ liệu từ bảng lưu kết quả
-        String getDataUser = "select * from results where user_id = "+userId;
-        Cursor cursor = dataBase.QueryGetData(getDataUser);
-        int maxRs = 0;
-        while (cursor.moveToNext()) {
-            int result = cursor.getInt(2);
-            if(result>maxRs){
-                maxRs = result;
-            }
-        }
-        txtRsMax.setText("Max result: "+maxRs);
-    }
-
-    private void getUserData() {
-        Intent intent = this.getIntent();
-        userId = intent.getIntExtra("user_id",0);
-        Toast.makeText(this, userId+"", Toast.LENGTH_SHORT).show();
-    }
-
-
-    private void map(){
-        btnThithu = (Button) findViewById(R.id.btnThithu);
-        btnStart = (Button) findViewById(R.id.start);
-        txtRs = (TextView) findViewById(R.id.txtRs);
-        txtRsMax = (TextView) findViewById(R.id.txtRsMax);
-        btnTrain = (Button) findViewById(R.id.btnTrain);
-        btnHistory = (Button) findViewById(R.id.btnHistory);
-        btnMore = (Button) findViewById(R.id.btnMore);
-    }
-
 }
