bd5a04f96aebd47240a63e24a94e09c38034905a
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-30 17:07:32 +0700

    Added navigation drawer

--- DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
+++ DrivingTestApp/app/src/main/java/com/example/drivingtestapp/MainActivity.java
@@ -5,11 +5,23 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Button btnExam, btnMore;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +48,46 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_main) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_thithusathach) {
+            startActivity(new Intent(this, ExamListActivity.class));
+        }
+//        else if (id == R.id.nav_hoclythuyet) {
+//            startActivity(new Intent(this, TraDacBietActivity.class));
+//        } else if (id == R.id.nav_tracuuluat) {
+//            startActivity(new Intent(this, BanhNgotActivity.class));
+//        } else if (id == R.id.nav_bienbao) {
+//            startActivity(new Intent(this, BanhManActivity.class));
+//        } else if (id == R.id.nav_tracuuluat) {
+//            startActivity(new Intent(this, CaiDatActivity.class));
+//        }
+        else if (id == R.id.nav_thoat) {
+            exit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void exit() {
+        finishAffinity();
+        System.exit(0);
+    }
+
 }
